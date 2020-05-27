758e31a082b58f35a45ed2167e05f199b11ded67
BlackCoffee <zbattleman@gmail.com>
2020-05-27 08:53:19 -0700

    signatures

--- main.go
+++ main.go
@@ -6,13 +6,23 @@ import (
 	"time"
 
 	"github.com/BlackCoffee77/KOTH-Memekit/fakeflags"
+	"github.com/BlackCoffee77/KOTH-Memekit/signatures"
 )
 
 func main() {
 	rand.Seed(int64(time.Now().Nanosecond()))
 	fakeFlagsPtr := flag.Int("fakeflags", 0, "Do you want to add fake flags?")
 
+	signaturePtr := flag.String("signature", "", "Whose signature would you like to use")
 	flag.Parse()
 
-	fakeflags.GenFakeFlags(*fakeFlagsPtr, "/")
+	if *fakeFlagsPtr > 0 {
+		fakeflags.GenFakeFlags(*fakeFlagsPtr, "/")
+	}
+
+	switch *signaturePtr {
+	case "example":
+		signatures.ExampleSignature()
+	}
+
 }
---
+++ signatures/Example.go
@@ -0,0 +1,8 @@
+package signatures
+
+import "fmt"
+
+//Example Signature given
+func ExampleSignature() {
+	fmt.Println("this is a signature for the user: example")
+}
