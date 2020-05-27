b60f7daaf0d6ebaca3ecf593c528749162af4fd6
BlackCoffee <zbattleman@gmail.com>
2020-05-27 07:03:09 -0700

    pensive

--- go.mod
+++
@@ -1,5 +0,0 @@
-module KOTH-Memekit
-
-go 1.14
-
-require github.com/BlackCoffee77/KOTH-Memekit v0.0.0-20200527132956-13feafcdf1b3 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,70 @@
 package main
 
 import (
+	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
+	"os"
 	"time"
-
-	"github.com/BlackCoffee77/KOTH-Memekit/memetools"
 )
+
+func genFlagName() string {
+	names := []string{"user", "flag", "king", "root", "congrats", "your_flag"}
+	ext := []string{".txt", ".md", "", ".lol"}
+
+	return names[rand.Intn(len(names))] + ext[rand.Intn(len(ext))]
+}
+
+// GenFakeFlags fake flag generator
+func genFakeFlags(num int, root string) {
+
+	fmt.Println("Placing fake flags at: ")
+	for i := 0; i < num; i++ {
+
+		depth := rand.Intn(10) + 1
+		curDir := root
+
+		for j := 0; j < depth; j++ {
+
+			files, err := ioutil.ReadDir(curDir)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			dirs := []string{}
+
+			for _, file := range files {
+				if file.IsDir() {
+					dirs = append(dirs, file.Name()+"/")
+				}
+			}
+
+			if len(dirs) == 0 {
+				//break in order to write file
+				break
+			}
+
+			curDir = curDir + dirs[rand.Intn(len(dirs))]
+		}
+
+		// create file at current location
+		loc := curDir + genFlagName()
+		fmt.Println(loc)
+		f, _ := os.Create(loc)
+		w := bufio.NewWriter(f)
+		bytes := md5.Sum([]byte(time.Now().String()))
+		finalHash := hex.EncodeToString(bytes[:])
+
+		w.WriteString(finalHash + "\n")
+		w.Flush()
+	}
+}
 
 func main() {
 	rand.Seed(int64(time.Now().Nanosecond()))
@@ -14,5 +72,5 @@ func main() {
 
 	flag.Parse()
 
-	memetools.GenFakeFlags(*fakeFlagsPtr, "/")
+	genFakeFlags(*fakeFlagsPtr, "/")
 }
